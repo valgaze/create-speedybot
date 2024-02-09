eaab53de6e7d0eb73c1e3527995c4fffb6117124
Victor Algaze <valgaze@gmail.com>
2024-02-08 17:33:11 -0800

    version bump, add node version force

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,34 @@
 #!/usr/bin/env node
+/* eslint-disable unicorn/prefer-number-properties */
+;(async () => {
+  // eslint-disable-next-line unicorn/no-array-callback-reference
+  const [major] = process.versions.node.split('.').map(parseFloat)
+  if (major < 18) {
+    console.log(`
+❌ ERROR!
 
-// eslint-disable-next-line unicorn/prefer-top-level-await
-(async () => {
+Your Node version is ${major}, SpeedyBot's cli requires at least node v18+
+
+You need to upgrade your node version, you can do that using any method you want.
+
+Option 1: Download + install Node from the official site: https://nodejs.org/en/download
+
+or
+
+Option 2: Download Node LTS with using Volta in the terminal: https://docs.volta.sh/guide/
+
+$ curl https://get.volta.sh | bash
+
+$ volta install node
+
+
+❌ ERROR! SpeedyBot will now exit
+
+`)
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+  }
+
   const oclif = await import('@oclif/core')
   await oclif.execute({development: false, dir: __dirname})
 })()
